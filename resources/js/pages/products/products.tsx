efa1d18daaf83f2f5ee33ba81efdea72b624a652
Aminat-Maker <aminatbolanle.ogidilawal@my.jcu.edu.au>
2025-02-28 14:09:31 +0300

    add products page

---
+++ resources/js/pages/products/products.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Navbar from "@/components/navbar";
+import Footer from "@/components/Footer";
+import CookieConsent from "@/components/ui/CookieConsent";
+import Contact from "@/components/contact";
+import SubHero from "@/components/subhero";
+import ProductList from "@/components/product-list";
+
+export default function Products() {
+    return (
+        <div>
+            {/* ========== HEADER ========== */}
+            <Navbar />
+            {/* ========== END HEADER ========== */}
+            {/* Hero */}
+            <SubHero
+                title="PROMINENT COLOR RANGE"
+                bgImage="/images/hero3.jpg"
+            />
+            <ProductList />
+            {/* Contact Us */}
+            <Contact title="Send An Inquiry" subtitle="" />
+            {/* End Contact Us */}
+            <Footer />
+            <CookieConsent variant="small" />
+        </div>
+    );
+}
